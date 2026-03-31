c659e53ddf717d1c1fe9e409d056bdfbdce7fc27
feredec <eldecsi@seznam.cz>
2026-03-31 04:32:54 +0200

    Update my_logger.hpp

--- DS18B20/my_logger.hpp
+++ DS18B20/my_logger.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
 #include <Arduino.h>
-
 #include <string>
 
 inline bool debug = true;
-inline bool resumeLedStateMonitor = false;
 
 void SerialLog(const char* msg);
 void SerialLog(const std::string& msg);
-
-void SerialLog_LedState(const char* state, unsigned long currMillis);
