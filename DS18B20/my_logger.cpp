ec3b688fa73c3e1f57937f480f0adc2e63238887
feredec <eldecsi@seznam.cz>
2026-03-31 04:32:28 +0200

    Update my_logger.cpp

--- DS18B20/my_logger.cpp
+++ DS18B20/my_logger.cpp
@@ -12,15 +12,5 @@ void SerialLog(const std::string& msg) {
   } 
 }
 
-void SerialLog_LedState(const char* state, unsigned long currMillis) {
-  if (!debug) return;
 
-  char buffer[32];
-  //const char* state = (newLedState == LED_ON) ? "\nON: " : "\nOFF: ";
-  int n = snprintf(buffer, sizeof(buffer), "%-5s:%8lu", state, currMillis);
-	if (n >= int(sizeof(buffer))) n = sizeof(buffer)-1;  // truncate
-	buffer[n] = '\0';
-
-  SerialLog(buffer);
-}
 
